e06d07836ba43a97b0a8ecd68e612355a0f38a0c
mrff88 <mrff88@gmail.com>
2021-12-14 17:50:30 -0500

    Add pokedex-react folder

---
+++ pokedex-react/src/components/pokemon/Pokemon.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { transformPokemon } from '../../utils';
+import Card from '../card/Card';
+
+const Pokemon = () => {
+  const { pokemonId } = useParams();
+  const [pokemon, setPokemon] = useState([]);
+
+  
+
+  useEffect(() => {
+    const getPokemonsFromApi = async () => {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+      ).then(res => res.json());
+  
+      setPokemon(transformPokemon(response));
+    };
+    getPokemonsFromApi();
+  }, [pokemonId]);
+
+  return (
+    <div>
+      {pokemon && (
+        <Card
+          image={pokemon.image}
+          name={pokemon.name}
+          ability={pokemon.ability}
+          id={pokemon.id}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Pokemon;
